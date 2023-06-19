0e6cbb47caffcdf4d4a3b5671a0f1e634aba286a
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-06-19 19:13:11 -0400

    vacas y toros

--- src/vectores.js
+++ src/vectores.js
@@ -26,4 +26,27 @@ function contarLetras(frase) {
   var resultado = contarLetras(frase);
   console.log("La frase tiene " + resultado + " letras (sin contar puntos ni comas).");
   
+
+  function adivinarNumero(adivinar, ingresado) {
+    let resultado = '';
+  
+    for (let i = 0; i < adivinar.length; i++) {
+      if (adivinar[i] === ingresado[i]) {
+        resultado += '!';
+      } else if (ingresado.includes(adivinar[i])) {
+        resultado += '#';
+      } else {
+        resultado += '_';
+      }
+    }
+  
+    return resultado;
+  }
+
+  const adivinanza = '1234';
+const intento = '1896';
+const resultado = adivinarNumero(adivinanza, intento);
+console.log(resultado); // Salida: "!___"
+
+
   module.exports = {titulo,contenido,posts};
