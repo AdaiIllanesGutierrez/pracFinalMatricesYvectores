fb6fd6c13839305c1d7516aaa5e9ba92183e652f
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-06-19 19:21:47 -0400

    matriz cypress

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -7,3 +7,24 @@ describe("Sumador", () => {
     cy.get("#resultado-div").should("contain", "9");
   });
 });
+describe('Crear matriz', () => {
+  it('Crea una matriz con la cantidad de filas y columnas especificadas', () => {
+    // Define los valores de filas y columnas
+    const filas = 3;
+    const columnas = 4;
+
+    // Visita la página donde se encuentra el programa JavaScript
+    cy.visit('/');
+
+    // Ejecuta el programa JavaScript en la consola del navegador
+    cy.window().then((win) => {
+      const matriz = win.crearMatriz(filas, columnas);
+
+      // Verifica que la matriz tenga el número correcto de filas y columnas
+      expect(matriz.length).to.equal(filas);
+      matriz.forEach((fila) => {
+        expect(fila.length).to.equal(columnas);
+      });
+    });
+  });
+});
